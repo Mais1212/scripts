e9e48faf3ce459fb86e418fc6bcfaad91b6dec5a
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2021-08-05 19:37:03 +0300

    Made a copy paste in the function

--- main.py
+++ main.py
@@ -2,31 +2,37 @@ from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
                                Schoolkid)
 
 
-def fix_marks(kid_name):
+def check_for_exist(kid_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
     except Schoolkid.DoesNotExist:
         print("Вероятно, вы ввели имя несуществующего ученика, попробуйте еще раз.")
+        return None
+    return schoolkid
+
+
+def fix_marks(kid_name):
+    schoolkid = check_for_exist(kid_name)
+    if schoolkid is None:
         return
-    bad_marks = Mark.objects.filter(
-        schoolkid=schoolkid, points__in=[2, 3])
+    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
     if not bad_marks:
-        print("Скорее всего, вы ввели имя ученика, у которого нет плохих оценок или такого имени.")
+        print("Скорее всего, вы ввели имя ученика, у которого нет плохих оценок")
         return
-    bad_marks.update(points=5)
+    for bad_mark in bad_marks:
+        bad_mark.points = 5
+        bad_mark.save()
     print("Оценки исправлены!")
 
 
 def remove_chastisements(kid_name):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
-    except Schoolkid.DoesNotExist:
-        print("Вероятно, вы ввели имя несуществующего ученика, попробуйте еще раз.")
+    schoolkid = check_for_exist(kid_name)
+    if schoolkid is None:
         return
     chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     if not chastisements:
         print(
-            "Скорее всего, вы ввели имя ученика, у которого нет замечани.")
+            "Скорее всего, вы ввели имя ученика, у которого нет замечаний.")
         return
     chastisements.delete()
     print("Замечания удалены!")
@@ -41,12 +47,10 @@ def append_commendation(subject, kid_name):
     date = lesson.date
     teacher = lesson.teacher
     subject = lesson.subject
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
-        Commendation.objects.create(text="Ты натренировался, брат.",
-                                    created=date, schoolkid=schoolkid,
-                                    teacher=teacher, subject=subject)
-    except Schoolkid.DoesNotExist:
-        print("Вероятно, вы ввели имя несуществующего ученика, попробуйте еще раз.")
-    finally:
-        print("Похвала добавлена!")
+    schoolkid = check_for_exist(kid_name)
+    if schoolkid is None:
+        return
+    Commendation.objects.create(text="Ты натренировался, брат.",
+                                created=date, schoolkid=schoolkid,
+                                teacher=teacher, subject=subject)
+    print("Похвала добавлена!")
